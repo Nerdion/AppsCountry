4dd7bd060637ea96352c2274c601274316ad212f
Neel Khalade <neel99khalade@gmail.com>
2020-06-06 22:32:45 +0530

    getting basic data

--- scraping/index.js
+++ scraping/index.js
@@ -1,14 +1,31 @@
 const puppeteer = require('puppeteer');
 
+let appName = 'TikTok';
+
 (async () => {
-    let url = "https://google.com";
+    let url = "https://google.com/search?q=" + appName;
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
+    await page.setViewport({
+        width: 1080,
+        height: 720,
+        deviceScaleFactor: 1,
+      });
     await page.goto(url,{waitUntil: 'networkidle2'});
+    await page.screenshot({path: 'ss/example.png'});
 
-    await page.waitFor("input[name='q']");
-    await page.evaluate(() => {
+    let myData = await page.evaluate(() => {
+        let topResults = document.querySelectorAll('.st');
+        var result = [];
+        for(let i=0; i<topResults.length; i++) {
+            result.push(topResults[i].innerText);
+        }
+
+        return {
+            result
+        }
     });
 
+    console.log(myData);
     await browser.close();
 })();
