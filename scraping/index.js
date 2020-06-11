2f35c4d6f9c0ae9771eebec68a95416578fa81bb
shriyashshingare <shriyashshingare@gmail.com>
2020-06-12 00:51:08 +0530

    search result organized

--- scraping/index.js
+++ scraping/index.js
@@ -1,14 +1,26 @@
-
-const GoogleSearch  = require('./models/GoogleSearch');
+const puppeteer = require('puppeteer');
+const GoogleSearch = require('./models/GoogleSearch');
 const PlayStore = require('./models/PlayStore');
-var gs = new GoogleSearch();
 
 async function myFunc() {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.setViewport({
+		width: 720,
+		height: 480,
+		deviceScaleFactor: 1,
+	});
+	var t1 = console.time()
 	//var data =  await gs.getSearchData()
-	var playStoreData = await new PlayStore().getPlayStoreInfo();
-	var googleSearch = await new GoogleSearch().getSearchData();
-	console.log(playStoreData);
-	console.log(googleSearch);
+	//var playStoreData = await new PlayStore().getPlayStoreInfo();
+	var googleSearch = await new GoogleSearch(browser,page).getSearchTermsResult("TikTok", browser);
+
+	//console.log(playStoreData);
+	//console.log(googleSearch);
+	var countryData=await new GoogleSearch().getCountryName()
+	await browser.close();
+	console.timeEnd()
+
 }
 
 myFunc();
--- scraping/models/GoogleSearch.js
+++ scraping/models/GoogleSearch.js
@@ -1,27 +1,34 @@
-const puppeteer = require('puppeteer');
 const countryDetector = require("country-in-text-detector");
 
-module.exports =  class GoogleSearch {
-	constructor() {
-		//   var searchdata=await this.getSearchData();
-		//   console.log("result data",searchdata);
-		//   var countrydata=await this.getCountryName(searchdata);
-		//   console.log("country name data",countrydata)
+module.exports = class GoogleSearch {
+	constructor(browser, page) {
+		this.browser = browser
+		this.page = page
 	}
 
-	async getSearchData() {
-		let appName = 'com.brave.browser';
-		let url = "https://google.com/search?q=" + appName;
-		const browser = await puppeteer.launch();
-		const page = await browser.newPage();
-		await page.setViewport({
-			width: 1080,
-			height: 720,
-			deviceScaleFactor: 1,
-		});
-		await page.goto(url, { waitUntil: 'networkidle2' });
-		console.log("outside")
-		let myData = await page.evaluate(() => {
+	async getSearchTermsResult(name) {
+		var resultData = [];
+		var searchterm = ["", "country", "headquarters country", "owners country", "parent company"];
+		var priority = [0.1, 0.3, 0.4, 0.6, 0.8]
+		for (let i = 0; i < 1; i++) {
+			let data = await this.getSearchData(name + " " + searchterm[i])
+			let a = `${searchterm[i]}_Result`;
+			resultData.push({
+				key: a,
+				Result: data,
+				priority: priority[i]
+			})
+		}
+		var countryData = await this.getCountryName(resultData)
+		return countryData;
+	}
+	async getSearchData(name) {
+		//let appName = 'com.brave.browser';
+		let url = "https://google.com/search?q=" + name;
+		//const browser = await puppeteer.launch();
+		await this.page.goto(url, { waitUntil: 'networkidle2' });
+		console.log("page")
+		let myData = await this.page.evaluate(() => {
 			let topResults = document.querySelectorAll('.st');
 			let appDescription = document.querySelector('#kp-wp-tab-overview > div:nth-child(1)');
 			var result = [];
@@ -29,25 +36,26 @@ module.exports =  class GoogleSearch {
 				result.push(topResults[i].innerText);
 			}
 			result.push(appDescription);
-
-			return {
-				result
-			}
+			return result
 		});
-		
-        await browser.close();
+
+		//await browser.close();
 		return myData;
 	}
 
 	async getCountryName(result) {
 		var country = [];
 		for (let i = 0; i < result.length; i++) {
-			let data = countryDetector.detect(result[i])
-			country.push(data)
+			var data = []
+			var phraseData = result[i].Result;
+			for (let j = 0; j < phraseData.length; j++) {
+				if (phraseData[j] != '') {
+					data.push(countryDetector.detect(phraseData[j]))
+				}
+			}
 		}
 		return country;
 	}
-
 	async hellWorld() {
 		await console.log("working");
 	}
