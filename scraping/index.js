d4907ea5291f3e9f6d45354615ee5327ab43649c
Neel Khalade <neel99khalade@gmail.com>
2020-06-11 19:02:45 +0530

    playstore class added

--- scraping/index.js
+++ scraping/index.js
@@ -1,10 +1,12 @@
 
 const GoogleSearch  = require('./models/GoogleSearch');
-
-var gs = new GoogleSearch()
+const PlayStore = require('./models/PlayStore');
+var gs = new GoogleSearch();
+var ps = new PlayStore();
 
 async function myFunc() {
-	var data =  await gs.getSearchData()
+	//var data =  await gs.getSearchData()
+	var playStoreData = await ps.getPlayStoreInfo();
 }
 
 myFunc();
--- scraping/models/PlayStore.js
+++ scraping/models/PlayStore.js
@@ -1,21 +1,37 @@
 const puppeteer = require('puppeteer');
 
-packageName = 'com.nextbillion.groww';
+module.exports =  class PlayStore {
+    constructor() {
+    }
 
-(async () => {
-    console.log("here");
-    let url = "https://play.google.com/store/apps/details?id=" + packageName;
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.setViewport({
-        width: 1080,
-        height: 720,
-        deviceScaleFactor: 1,
-    });
-    console.log("here");
-    await page.goto(url,{waitUntil: 'networkidle2'});
-    await page.waitUntil(5000);
-    await page.screenshot({path: 'ss/example.png'});
-    
-    await browser.close();
-});
+    async getPlayStoreInfo() {
+        let url = "https://play.google.com/store/apps/details?id=" + 'com.brave.browser';
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.setViewport({
+            width: 1080,
+            height: 720,
+            deviceScaleFactor: 1,
+        });
+        await page.goto(url, { waitUntil: 'networkidle2' });
+        await page.screenshot({ path: 'ss/example.png' });
+        await page.waitForSelector("a[href*='/store/apps/dev']");
+        var developerName = await page.evaluate(() => { 
+            return document.querySelectorAll("a[href*='/store/apps/dev']")[0].textContent;
+        });
+        
+        var appInfo = await page.evaluate(() => {
+            var myVar = Array.from(document.querySelectorAll('.hAyfc:last-child > span > div > span > div').map(
+                myData => myData.innerText.trim()
+            ));
+
+            return {
+                myVar
+            }
+        });
+
+        await console.log(appInfo);
+        await console.log(developerName);
+        await browser.close();
+    }
+}
